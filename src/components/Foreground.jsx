a1f4e59478be198c3a10ea31efe2993a081daf79
chiragjoshi7 <chiragjoshi45112001gmail.com>
2024-04-14 11:44:37 +0530

    first commit

---
+++ src/components/Foreground.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Card from './Card';
+
+const Foreground = () => {
+
+    const data = [
+        {
+           desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+           filesize:".9mb", 
+           close: false,
+           tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green"},
+        },
+
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+            filesize:".9mb", 
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue"},
+        },
+
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+            filesize:".9mb", 
+            close: true,
+            tag: { isOpen: true, tagTitle: "Upload", tagColor: "green"},
+        },
+
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+            filesize:".9mb", 
+            close: true,
+            tag: { isOpen: false, tagTitle: "Upload", tagColor: "green"},
+        },
+ ];
+
+  return (
+    <div className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
+       {data.map((item, index)=>(
+        <Card data={item} />
+       ))}
+    </div>
+  )
+}
+
+export default Foreground
